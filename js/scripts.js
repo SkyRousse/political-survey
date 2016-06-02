e01dfece9e2c468e1086c715ae72d892bde633e4
Kevin VanEvery and Sky Rousse <kevin@>
2016-06-02 11:08:46 -0700

    setup form to gather political prefs and add js to calculate a number total to reveal political preference

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,21 @@
 /*This file is for your custom js.  All yours*/
+$(function() {
+  $("form#political-survey").submit(function(event){
+    event.preventDefault();
+    var globalWarming = parseInt($("input[name=global-warming]:checked").val());
+    var gunCheck = parseInt($("input[name=gun-check]:checked").val());
+    var marijuana = parseInt($("input[name=marijuana]:checked").val());
+    var military = parseInt($("input[name=military]:checked").val());
+    var score = globalWarming + gunCheck + marijuana + military;
+    debugger;
+    if (score <= 1) {
+      $("#red").show();
+    }
+    else if (score === 2) {
+      $("#middle").show();
+    }
+    else {
+      $("#blue").show();
+    }
+  });
+});
